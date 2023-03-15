f4789af63dde4397d01c3843f87de17813bb0875
Filipe Borges <filipborgs48@gmail.com>
2023-03-15 20:30:11 -0300

    feat: server config

--- gamer-api/src/main/config/env.ts
+++ gamer-api/src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  clientId: process.env.CLIENT_ID_IGDB ?? 's9xeh7eekz5cucxb32ze57grvr98v4',
-  token: process.env.TOKEN_IGDB ?? '6pd0e5dhpkt4nl4ca4ymjyu4jmo4bh',
+  clientId: process.env.CLIENT_ID_IGDB ?? '',
+  secret: process.env.SECRET_IGDB ?? '',
   port: process.env.PORT ?? 5050
 }
--- gamer-api/src/main/server.ts
+++ gamer-api/src/main/server.ts
@@ -1,8 +1,7 @@
 import 'module-alias/register'
-import { IgdbHelper } from '@/infra/api/games/helpers'
 import env from '@/main/config/env'
 
-void IgdbHelper.connect(env.clientId, env.token).then(async () => {
+void (async function () {
   const app = (await import('./config/app')).default
   app.listen(env.port, () => { console.log(`Server is running ${env.port}`) })
-})
+}())
