07acc5100465cd39872520aedf17110cd46fe910
Filipe Borges <filipborgs48@gmail.com>
2023-03-15 22:14:53 -0300

    chore: add dotenv

--- gamer-api/src/main/server.ts
+++ gamer-api/src/main/server.ts
@@ -1,7 +1,9 @@
 import 'module-alias/register'
-import env from '@/main/config/env'
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 void (async function () {
   const app = (await import('./config/app')).default
+  const env = (await import('./config/env')).default
   app.listen(env.port, () => { console.log(`Server is running ${env.port}`) })
 }())
