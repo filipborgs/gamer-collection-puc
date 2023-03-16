954c0f43c7cbb9c3270a7454a674065a02c19040
Filipe Borges <filipborgs48@gmail.com>
2023-03-16 20:14:49 -0300

    feat: add LoadPlatformsController

---
+++ gamer-api/src/presentation/controllers/platforms/load-platforms-controller.ts
@@ -0,0 +1,17 @@
+import { type LoadPlatforms } from '@/domain/usecases/platforms'
+import { ok, serverError } from '@/presentation/helpers/http'
+import { type Controller, type HttpRequest, type HttpResponse } from '@/presentation/protocols'
+
+export class LoadPlatformsController implements Controller {
+  constructor (private readonly loadPlatforms: LoadPlatforms) {}
+
+  async handle ({ query }: HttpRequest): Promise<HttpResponse> {
+    try {
+      const { search, offset } = query
+      const platforms = await this.loadPlatforms.load({ offset, search })
+      return ok(platforms)
+    } catch (error) {
+      return serverError(error)
+    }
+  }
+}
