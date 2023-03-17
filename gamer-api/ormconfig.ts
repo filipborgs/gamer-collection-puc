763aef679c399ad2d1d0d723b4ed2eff85ed956b
Filipe Borges <filipborgs48@gmail.com>
2023-03-17 14:54:59 -0300

    chore: add typeorm

---
+++ gamer-api/ormconfig.ts
@@ -0,0 +1,17 @@
+import * as dotenv from 'dotenv'
+import { DataSource } from 'typeorm'
+dotenv.config()
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  synchronize: true,
+  logging: true,
+  entities: [`${process.env.TS_NODE_DEV === undefined ? 'dist' : 'src'}/infra/repos/postgres/entities/index.{js,ts}`],
+  migrations: [`${process.env.TS_NODE_DEV === undefined ? 'dist' : 'src'}/infra/repos/postgres/migrations/*.{js,ts}`],
+  subscribers: []
+})
