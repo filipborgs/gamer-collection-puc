0483669bf56cadabc85589f81ce76c8ac658f472
Filipe Borges <filipborgs48@gmail.com>
2023-03-21 22:21:06 -0300

    test: add BcryptAdapter

---
+++ gamer-api/test/infra/criptography/bcrypt-adapter.spec.ts
@@ -0,0 +1,38 @@
+import bcrypt from 'bcrypt'
+import { BcryptAdapter } from '@/infra/criptography'
+
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return await Promise.resolve('hashed_string')
+  }
+}))
+
+describe('Bcrypt Adapter', () => {
+  let sut: BcryptAdapter
+  const salt = 12
+
+  beforeEach(() => {
+    sut = new BcryptAdapter(salt)
+  })
+
+  describe('hash()', () => {
+    it('should call hash with correct values', async () => {
+      const param = 'value'
+      const hashSpy = jest.spyOn(bcrypt, 'hash')
+      await sut.hash(param)
+      expect(hashSpy).toHaveBeenCalledWith(param, salt)
+    })
+
+    it('should return a hash on sucess', async () => {
+      const hash = await sut.hash('any_string')
+      expect(hash).toBe('hashed_string')
+    })
+
+    it('shoud throw if hash throws', async () => {
+      const error = new Error('any_error')
+      jest.spyOn(bcrypt, 'hash').mockRejectedValueOnce(error)
+      const promise = sut.hash('any_value')
+      await expect(promise).rejects.toThrow(error)
+    })
+  })
+})
