8823063a56183a749ca6ca3fbfdd4677a785372b
Filipe Borges <filipborgs48@gmail.com>
2023-03-22 21:47:16 -0300

    feat: add HashCompare implementation

--- gamer-api/src/infra/criptography/bcrypt-adapter.ts
+++ gamer-api/src/infra/criptography/bcrypt-adapter.ts
@@ -1,11 +1,16 @@
-import { type Hasher } from '@/data/protocols/criptography/hasher'
+import { type HashCompare, type Hasher } from '@/data/protocols/criptography'
 import bcrypt from 'bcrypt'
 
-export class BcryptAdapter implements Hasher {
+export class BcryptAdapter implements Hasher, HashCompare {
   constructor (private readonly salt: number) {}
 
   async hash (value: string): Promise<string> {
     const hashedValue = await bcrypt.hash(value, this.salt)
     return hashedValue
+  }
+
+  async compare (value: string, hash: string): Promise<boolean> {
+    const isEqual = await bcrypt.compare(value, hash)
+    return isEqual
   }
 }
