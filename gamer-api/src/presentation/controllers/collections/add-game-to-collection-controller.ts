e15e4a485b2af87b3f92c6fa89d6efd3c2e3018a
Filipe Borges <filipborgs48@gmail.com>
2023-04-04 20:48:04 -0300

    feat: add game item controller

---
+++ gamer-api/src/presentation/controllers/collections/add-game-to-collection-controller.ts
@@ -0,0 +1,18 @@
+import { type AddItemToCollection } from '@/domain/usecases/collection'
+import { NotFound } from '@/presentation/errors'
+import { created, notFound, serverError } from '@/presentation/helpers/http'
+import { type Controller, type HttpRequest, type HttpResponse } from '@/presentation/protocols'
+
+export class AddGameToCollectionController implements Controller {
+  constructor (private readonly loadGame: AddItemToCollection) {}
+
+  async handle ({ body }: HttpRequest): Promise<HttpResponse> {
+    try {
+      const id = await this.loadGame.add(body)
+      if (!id) return notFound(new NotFound('Game'))
+      return created(id)
+    } catch (error) {
+      return serverError(error)
+    }
+  }
+}
