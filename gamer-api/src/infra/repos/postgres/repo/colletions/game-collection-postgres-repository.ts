d6fb1be2549b7a7fc871d93bf2a24ea187107428
Filipe Borges <filipborgs48@gmail.com>
2023-04-04 20:48:44 -0300

    feat: add game collection repo

---
+++ gamer-api/src/infra/repos/postgres/repo/colletions/game-collection-postgres-repository.ts
@@ -0,0 +1,21 @@
+import { type AddGameItemParamsRepo, type AddGameItemToCollectionRepository } from '@/data/protocols/repo/collections'
+import { type Repository } from 'typeorm'
+import { CollectionGameItemTypeOrm } from '../../entities/collection-game-item-typeorm'
+import { PgRepository } from '../../helpers'
+
+export class GameCollectionPostgresRepository extends PgRepository implements AddGameItemToCollectionRepository {
+  private readonly repo: Repository<CollectionGameItemTypeOrm>
+  constructor () {
+    super()
+    this.repo = this.getRepository(CollectionGameItemTypeOrm)
+  }
+
+  async addGameItem (data: AddGameItemParamsRepo): Promise<string> {
+    const { id } = await this.repo.save({
+      ...data,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    })
+    return id
+  }
+}
