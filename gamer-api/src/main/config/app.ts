c2485c42dfe0397bf5e11377dfad352d72a094cb
Filipe Borges <filipborgs48@gmail.com>
2023-04-15 19:01:00 -0300

    chore: add cors

--- gamer-api/src/main/config/app.ts
+++ gamer-api/src/main/config/app.ts
@@ -1,10 +1,10 @@
 import express from 'express'
 import setupRoutes from '@/main/config/routes'
-
+import cors from 'cors'
 const app = express()
 
 app.use(express.json())
-
+app.use(cors())
 setupRoutes(app)
 
 export default app
