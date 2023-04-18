782dadf7a4d1ea33a489c3ba6d75bd3de915610a
Filipe Borges <filipborgs48@gmail.com>
2023-04-18 20:28:32 -0300

    refactor: move game item page to separeted file

--- gamer-collection-front/pages/game/_id.vue
+++ gamer-collection-front/pages/game/_id.vue
@@ -1,22 +1,126 @@
 <template>
-  <ItemPage :service="gameService" />
+  <v-container>
+    <v-row>
+      <v-col cols="12" sm="3">
+        <v-sheet color="grey darken-4" rounded="lg" min-height="268">
+          <v-card class="mx-auto" max-width="400">
+            <AddCollectionItem />
+            <v-img class="rounded-lg" :src="image" height="400px" dark>
+              <template #placeholder>
+                <v-row class="fill-height ma-0" align="center" justify="center">
+                  <v-progress-circular
+                    indeterminate
+                    color="grey lighten-5"
+                  ></v-progress-circular>
+                </v-row>
+              </template>
+            </v-img>
+
+            <v-card-title> {{ item.name }} </v-card-title>
+            <v-card-subtitle> {{ releaseDate }} </v-card-subtitle>
+            <v-card-subtitle> {{ platforms }} </v-card-subtitle>
+            <br/>
+          </v-card>
+        </v-sheet>
+      </v-col>
+
+      <v-col cols="12" sm="6">
+        <v-sheet color="grey darken-4" min-height="70vh" rounded="lg">
+          <v-card class="mx-auto">
+            <v-card dark flat>
+              <v-card-title class="pa-2 purple lighten-3">
+                <h3 class="text-h6 font-weight-light text-center grow">
+                  Minhas coleções
+                </h3>
+              </v-card-title>
+            </v-card>
+            <v-card-text class="py-0">
+              <v-timeline align-top dense>
+                <v-timeline-item color="pink" small>
+                  <v-row class="pt-1">
+                    <v-col cols="3">
+                      <strong>PS2</strong>
+                    </v-col>
+                    <v-col>
+                      <strong>Valor: R$: 22,30</strong>
+                      <div class="text-caption">Comprado em: 11/11/2003</div>
+                    </v-col>
+                  </v-row>
+                </v-timeline-item>
+
+                <v-timeline-item color="pink" small>
+                  <v-row class="pt-1">
+                    <v-col cols="3">
+                      <strong>GC</strong>
+                    </v-col>
+                    <v-col>
+                      <strong>Valor: R$: 50,30</strong>
+                      <div class="text-caption">Comprado em: 11/11/2003</div>
+                    </v-col>
+                  </v-row>
+                </v-timeline-item>
+              </v-timeline>
+            </v-card-text>
+          </v-card>
+        </v-sheet>
+      </v-col>
+
+      <v-col cols="12" sm="3">
+        <v-sheet color="grey darken-4" rounded="lg" min-height="268">
+          <v-card-title>
+            <p>Outras versões</p>
+          </v-card-title>
+
+          <v-card-text>
+            <v-list two-line>
+              <v-list-item-group
+                v-model="selected"
+                active-class="pink--text"
+                multiple
+              >
+                <template v-for="(ex, index) in items">
+                  <v-list-item :key="ex.title">
+                    <v-list-item-content>
+                      <v-list-item-title>{{ ex.title }}</v-list-item-title>
+
+                      <v-list-item-subtitle class="text--primary">{{
+                        ex.headline
+                      }}</v-list-item-subtitle>
+
+                      <v-list-item-subtitle>{{
+                        ex.subtitle
+                      }}</v-list-item-subtitle>
+                    </v-list-item-content>
+                  </v-list-item>
+
+                  <v-divider
+                    v-if="index < ex.length - 1"
+                    :key="index"
+                  ></v-divider>
+                </template>
+              </v-list-item-group>
+            </v-list>
+          </v-card-text>
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 import { makeApiLoadGameById } from '../../app/main/factories/domain/usecases/game'
-import ItemPage from '../../components/layout/item-page.vue'
+import AddCollectionItem from '../../components/collection/add-game-collection-item.vue'
 
 export default {
-  components: {
-    ItemPage
-  },
+  name: 'GamePage',
+  components: { AddCollectionItem },
   data: () => ({
-    game: {
+    item: {
       id: null,
       name: null,
-      platforms: [],
       cover: {},
-      releaseDate: null
+      releaseDate: null,
+      platforms: []
     },
     selected: [2],
     items: [
@@ -28,7 +132,31 @@ export default {
       }
     ],
     gameService: makeApiLoadGameById()
-  })
+  }),
+
+  computed: {
+    releaseDate() {
+      const releaseDate = this.item.releaseDate
+      return releaseDate ? new Date(releaseDate).getFullYear() : ''
+    },
+
+    image() {
+      const code = this.item.cover.code
+      return code
+        ? `https://images.igdb.com/igdb/image/upload/t_cover_big/${code}.jpg`
+        : ''
+    },
+
+    platforms() {
+      return this.item.platforms.map(i => i.name).slice().join(' | ')
+    }
+  },
+
+  async mounted() {
+    const id = this.$route.params.id
+    const item = await this.gameService.loadById(id)
+    this.item = item
+  }
 }
 </script>
 
