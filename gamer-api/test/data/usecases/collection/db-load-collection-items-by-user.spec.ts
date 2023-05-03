39bc8c626096af3f63aeb577ba31c260df1540d9
Filipe Borges <filipborgs48@gmail.com>
2023-05-02 21:06:43 -0300

    test: add DbLoadGameCollectionItems

---
+++ gamer-api/test/data/usecases/collection/db-load-collection-items-by-user.spec.ts
@@ -0,0 +1,41 @@
+import { LoadGameCollectionItemsRepository } from '@/data/protocols/repo/collection'
+import { DbLoadGameCollectionItems } from '@/data/usecases/collection'
+import { LoadGameCollectionItemsParams } from '@/domain/usecases/collection'
+import { mockGameCollectionItem } from '@/test/domain/entities/mocks'
+import { MockProxy, mock } from 'jest-mock-extended'
+
+describe('DbLoadGameCollectionItems', () => {
+  let sut: DbLoadGameCollectionItems
+  let collectionsRepo: MockProxy<LoadGameCollectionItemsRepository>
+  let params: LoadGameCollectionItemsParams
+
+  beforeEach(() => {
+    collectionsRepo = mock<LoadGameCollectionItemsRepository>()
+    collectionsRepo.loadByUser.mockResolvedValue([mockGameCollectionItem()])
+
+    params = {
+      userId: 'any_id',
+      platformId: 2
+    }
+
+    sut = new DbLoadGameCollectionItems(collectionsRepo)
+  })
+
+  it('Should call LoadGameCollectionItemsRepository with userId and platformID', async () => {
+    await sut.loadByUser(params)
+    const { platformId, userId } = params
+    expect(collectionsRepo.loadByUser).toBeCalledWith({ platformId, userId })
+  })
+
+  it('Should throw an error if LoadGameCollectionItemsRepository throws', async () => {
+    const error = new Error('an error')
+    collectionsRepo.loadByUser.mockRejectedValueOnce(error)
+    const promise = sut.loadByUser(params)
+    await expect(promise).rejects.toThrow(error)
+  })
+
+  it('Should return an list of collections if successful', async () => {
+    const list = await sut.loadByUser(params)
+    expect(list).toEqual([mockGameCollectionItem()])
+  })
+})
