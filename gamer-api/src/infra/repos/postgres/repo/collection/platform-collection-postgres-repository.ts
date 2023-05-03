f7819af6400b55ff0e08b1065a11635188e14172
Filipe Borges <filipborgs48@gmail.com>
2023-05-02 21:51:06 -0300

    feat: add LoadPlatformCollectionItemsRepository implementation

--- gamer-api/src/infra/repos/postgres/repo/collection/platform-collection-postgres-repository.ts
+++ gamer-api/src/infra/repos/postgres/repo/collection/platform-collection-postgres-repository.ts
@@ -1,9 +1,10 @@
-import { AddPlatformItemToCollectionRepository, AddPlatfromItemParamsRepo } from '@/data/protocols/repo/collection'
+import { AddPlatformItemToCollectionRepository, AddPlatfromItemParamsRepo, LoadPlatformCollectionItemsRepository, LoadPlatformCollectionItemsRepositoryParams } from '@/data/protocols/repo/collection'
+import { PlatfromCollectionItem } from '@/domain/entities'
 import { PlatformCollectionItemTypeOrm } from '@/infra/repos/postgres/entities'
 import { PgRepository } from '@/infra/repos/postgres/helpers'
 import { Repository } from 'typeorm'
 
-export class PlatformCollectionPostgresRepository extends PgRepository implements AddPlatformItemToCollectionRepository {
+export class PlatformCollectionPostgresRepository extends PgRepository implements AddPlatformItemToCollectionRepository, LoadPlatformCollectionItemsRepository {
   private readonly repo: Repository<PlatformCollectionItemTypeOrm>
   constructor () {
     super()
@@ -15,6 +16,14 @@ export class PlatformCollectionPostgresRepository extends PgRepository implement
       ...data,
       createdAt: new Date(),
       updatedAt: new Date()
+    })
+  }
+
+  async loadByUser ({ userId }: LoadPlatformCollectionItemsRepositoryParams): Promise<PlatfromCollectionItem[]> {
+    return await this.repo.find({
+      where: {
+        userId
+      }
     })
   }
 }
