298dc056137e94b88687f4f8713e5d2b56e7d6a3
Filipe Borges <filipborgs48@gmail.com>
2023-07-22 21:47:24 -0300

    test: add load follows by follower id controller

---
+++ gamer-api/test/presentation/controllers/follow/load-follows-by-follower-id-controller.spec.ts
@@ -0,0 +1,50 @@
+import { LoadFollowsByFollowerId } from '@/domain/usecases/user'
+import { LoadFollowsByFollowerIdController } from '@/presentation/controllers/follow'
+import { noContent, ok, serverError } from '@/presentation/helpers/http'
+import { HttpRequest } from '@/presentation/protocols'
+import { mockFollowerRegistry } from '@/test/domain/entities/mocks'
+import { MockProxy, mock } from 'jest-mock-extended'
+
+describe('LoadFollowsByFollowerIdController', () => {
+  let sut: LoadFollowsByFollowerIdController
+  let loadFollows: MockProxy<LoadFollowsByFollowerId>
+  let httpRequest: HttpRequest
+
+  beforeEach(() => {
+    jest.useFakeTimers()
+    loadFollows = mock()
+    loadFollows.loadFollows.mockResolvedValue([mockFollowerRegistry()])
+    sut = new LoadFollowsByFollowerIdController(loadFollows)
+    httpRequest = makeHttpRequest()
+  })
+
+  const makeHttpRequest = (): HttpRequest => ({
+    params: {
+      followerId: 'any_id'
+    }
+  })
+
+  it('Should call LoadFollowsByFollowerId with followerId', async () => {
+    await sut.handle(httpRequest)
+    const { params: { followerId } } = httpRequest
+    expect(loadFollows.loadFollows).toHaveBeenCalledWith({ followerId })
+  })
+
+  it('Should return ServerError if LoadFollowsByFollowerId Throws', async () => {
+    const error = new Error('any')
+    loadFollows.loadFollows.mockRejectedValueOnce(error)
+    const response = await sut.handle(httpRequest)
+    expect(response).toEqual(serverError(error))
+  })
+
+  it('Should return 204 if LoadFollowsByFollowerId returns null', async () => {
+    loadFollows.loadFollows.mockResolvedValueOnce(null)
+    const response = await sut.handle(httpRequest)
+    expect(response).toEqual(noContent())
+  })
+
+  it('Should return 200 if succeds', async () => {
+    const response = await sut.handle(httpRequest)
+    expect(response).toEqual(ok([mockFollowerRegistry()]))
+  })
+})
